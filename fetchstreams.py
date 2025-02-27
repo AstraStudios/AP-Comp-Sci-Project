648bb1dbb0f93df7cdbe57060323abadad702b98
WonderingRatsRunThroughThePlants <luke.brittain+e@gmail.com>
2025-02-27 15:26:01 -0600

    remove C# add fetchstreams.py

---
+++ fetchstreams.py
@@ -0,0 +1,8 @@
+import spotipy # from https://spotipy.readthedocs.io/en/2.25.1/
+from spotipy.oauth2 import SpotifyClientCredentials
+import json
+
+# check spotify api and give client ID
+spotifyClientID = "3267860352f446608621449584cf92b2"
+spotifyClientSecret = "363931554d1a4f0eaca6fd88d10891f1"
+
